65a985c164ecd38bcdbc45d6b9c924654157de40
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2025-11-26 23:48:04 +0530

    some debugging in api-gateway

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -57,7 +57,7 @@ app.use(async (req, res, next) => {
 
 const sensitiveEndpointLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // limit each IP to 10 requests per windowMs
+    max: 100, // limit each IP to 10 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: (req, res) => {
