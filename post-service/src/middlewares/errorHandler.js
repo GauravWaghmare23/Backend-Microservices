1ac4e78495a84c645e0464282f2942bd18fd0338
Gaurav Waghmare <gauravwaghmare95032@gmail.com>
2025-11-28 22:29:37 +0530

    Update errorHandler.js
    created and updated and debugged the error handler

--- post-service/src/middlewares/errorHandler.js
+++ post-service/src/middlewares/errorHandler.js
@@ -1,1 +1,11 @@
-l
+const logger = require('../utils/logger.js')
+
+const errorHandler = (err,req,res,next) => {
+  logger.error(err.message);
+  res.
+    status(err.status || 500)
+    .json({message : err.message || "Internal server error"});
+  next();
+};
+
+module.exports = errorHandler;
