119824435ef325feb7312654a7bdff2b6e29f242
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2025-11-30 17:08:03 +0530

    created authentication middleware, error Handler and logger using winston for media service

---
+++ media-service/src/middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+const logger = require("../utils/logger");
+
+const authenticationMiddleware = async (req, res, next) => {
+  logger.info("Authentication middleware hit");
+  try {
+    const userId = req.headers["x-user-id"];
+
+    if (!userId) {
+      logger.warn("Access attempted without userId");
+      return res
+        .status(401)
+        .json({
+          success: false,
+          message: "Unauthorized - Please login or register to continue.",
+        });
+    }
+
+    req.user = {
+      userId,
+      _id: userId,
+    };
+
+    next();
+  } catch (error) {
+    logger.error(
+      `Authentication middleware error in media-service: ${error.message}`
+    );
+    const message =
+      process.env.NODE_ENV === "production"
+        ? "Internal Server Error"
+        : error.message;
+    res.status(500).json({ success: false, message: message });
+  }
+};
+
+
+module.exports = authenticationMiddleware;
---
+++ media-service/src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,12 @@
+const logger = require("../utils/logger");
+
+
+const errorHandler = async (err, req, res, next) => {
+    logger.error(err.message);
+    res
+        .status(err.status || 500)
+        .json({ success: false, message: err.message || "Internal Server Error" });
+    next();
+};
+
+module.exports = errorHandler;
