b26f7198297d9f16d757beade7370ba9839ef807
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2025-11-30 20:53:26 +0530

    added cloudinary

---
+++ media-service/src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+const cloudinary = require('cloudinary').v2;
+const { UploadStream } = require('cloudinary');
+const logger = require('./logger.js')
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadMediaToCloudinary = (file) => {
+    return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+            { resource_type: 'auto' }, (error, result) => {
+                if (error) {
+                    logger.error(error);
+                    reject(error);
+                } else {
+                    logger.info(`Media uploaded to Cloudinary: URL : ${result.secure_url}, Public ID : ${result.public_id}`);
+                    resolve(result);
+                }
+            }
+        )
+        uploadStream.end(file.buffer);
+    });
+}
+console.log(uploadMediaToCloudinary);
+
+module.exports = {
+    uploadMediaToCloudinary
+}
